b9cfa1ac5c8354cc29b424fbc660418d6f4c9080
Vishesh Singh <VisheshSingh16@gmail.com>
2021-04-20 21:49:54 -0500

    Single Event Page 1️⃣

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,55 @@
 import React from 'react';
 import { API_URL } from '@/config/globals';
 import Layout from '@/components/Layout';
+import styles from '@/styles/Event.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaPencilAlt, FaTrash } from 'react-icons/fa';
 
-const EventPage = ({ events }) => {
-  console.log({ events });
-  return <Layout title='Event page'></Layout>;
+const EventPage = ({ evt }) => {
+  const deleteHandler = () => {
+    console.log('delete');
+  };
+  return (
+    <Layout title={evt.name}>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit event
+            </a>
+          </Link>
+          <a href='#' onClick={deleteHandler} className={styles.delete}>
+            <FaTrash /> Delete event
+          </a>
+        </div>
+
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+      </div>
+
+      <Link href='/events'>
+        <a className={styles.back}>{'<'} Go Back</a>
+      </Link>
+    </Layout>
+  );
 };
 
 export default EventPage;
@@ -25,7 +70,7 @@ export async function getStaticProps({ params: { slug } }) {
 
   return {
     props: {
-      events: event[0],
+      evt: event[0],
       revalidate: 1,
     },
   };
