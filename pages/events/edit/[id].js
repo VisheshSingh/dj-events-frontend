6d6970e29c37f77ee2710fc9badaa98281fd0da3
Vishesh Singh <VisheshSingh16@gmail.com>
2021-04-24 10:57:30 -0500

    Image upload section 🖼

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import moment from 'moment';
 import Link from 'next/link';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import Layout from '@/components/Layout';
 import styles from '@/styles/Form.module.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { API_URL } from 'config/globals';
+import { FaImage } from 'react-icons/fa';
 
 const EditEvent = ({ evt }) => {
   const [values, setValues] = useState({
@@ -18,6 +20,10 @@ const EditEvent = ({ evt }) => {
     time: evt.time,
     description: evt.description,
   });
+
+  const [imagePreview, setImagePreview] = useState(
+    evt.image ? evt.image.formats.thumbnail.url : null
+  );
 
   const router = useRouter();
 
@@ -145,6 +151,21 @@ const EditEvent = ({ evt }) => {
 
         <input type='submit' value='Update Event' className='btn' />
       </form>
+
+      <h2>Event Image</h2>
+      {imagePreview ? (
+        <Image src={imagePreview} height={100} width={170} />
+      ) : (
+        <div>
+          <p>No image uploaded</p>
+        </div>
+      )}
+
+      <div>
+        <button className='btn-secondary'>
+          <FaImage /> Set Image
+        </button>
+      </div>
     </Layout>
   );
 };
